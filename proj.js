21fa357ae190af312b99807da9d9d257dc52e79e
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-06-14 21:29:59 +0530

    add filter and description

--- proj.js
+++ proj.js
@@ -1,18 +1,23 @@
 //alert("hello world");
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
+//let itemList2 =document.getElementById('secondItem')
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
-
+//filter function calling
+filter.addEventListener('keyup', filterItems);
+//filter.addEventListener('keyup', filterItems1);
 //function add item
 function addItem(e){
     e.preventDefault();
 
     //create input value
     let newItem=document.getElementById('item').value;
+    let it = document.getElementById('secondItem').value;
 
     //create new li
     let li=document.createElement('li');
@@ -20,6 +25,8 @@ function addItem(e){
     li.className="list-group-item";
    //add text node
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode( it));
+    console.log(li);
     //add del button
     let delbtn=document.createElement('button');
 
@@ -52,3 +59,46 @@ function removeItem(e){
       }
     }
   }
+
+  function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.childNodes[0].textContent;
+      var itemDescription = item.childNodes[1].textContent;
+      var combinedText = itemName.toLowerCase() + ' ' + itemDescription.toLowerCase();
+  
+      if (combinedText.indexOf(text) !== -1) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  } 
+      
+   
+/*function filterItems1(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+   
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.childNodes[1].textContent;
+     
+  
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } 
+      else{
+        item.style.display='none';
+      }  
+      
+    });
+  
+  }*/
